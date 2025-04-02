35314209f212878eba6f977fcbc418d4bf2b24ed
Bijit Mondal <bijitmondal3011@rediffmail.com>
2025-04-02 18:26:23 +0530

    build folder changed to dist

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,10 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2025-04-02"
+  compatibilityDate: "2025-04-02",
+    output: {
+    dir: 'dist',
+    publicDir: 'dist/public',
+    serverDir: 'dist/server'
+  }
 });
