b328b1c597154a271892116b1a54b32fddb10699
Bijit-Mondal <bijitmondal3011@rediffmail.com>
2025-04-03 21:45:42 +0530

    Aapki Bar Hamari Sarkar

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,7 +1,9 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2025-04-02"
+  compatibilityDate: "2025-04-02",
+  preset: "firebase_app_hosting",
+  moduleSideEffects: ["firebase-admin", "firebase-functions"],
   //  output: {
   //  dir: 'dist',
   //  publicDir: 'dist/public',
